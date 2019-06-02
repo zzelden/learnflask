67107058a3c89223c23269d741c4ce653be53f98
Denis Seleznev <selden.new@gmail.com>
2019-06-02 15:49:34 +0500

    python news

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
+from python_news import get_python_news
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    title = 'Новости python'
     weather = weather_by_city("Chelyabinsk")
-    if weather:
-        return f"Сейчас {weather['temp_C']}, ощущается как {weather['FeelsLikeC']}"
-    else:
-        return "Сервис погоды недоступен"
+    news_list = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -1,7 +1,9 @@
 import requests
 
+weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+
+
 def weather_by_city(city_name):
-    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
     params = {
         "key": "babf391873c14ff483a124221192705",
         "q": city_name,
@@ -9,15 +11,21 @@ def weather_by_city(city_name):
         "num_of_days": 1,
         "lang": "ru"
     }
-    result = requests.get(weather_url, params=params)
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except(IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except(IndexError, TypeError):
+                    return False
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
     return False
+
 
 if __name__ == "__main__":
     w = weather_by_city("Chelyabinsk")
